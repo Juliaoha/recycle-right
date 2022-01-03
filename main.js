8f28ed4a847796defecc06826c40c7fa775d2c52
@Juliaoha <juliaoha@hotmail.com>
2022-01-04 00:44:56 +1030

    Added id's to bins, event listener for drop

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-console.log("Hello World");
-const fay = "annoying";
-
-dragula([
+const drake = dragula([
   document.querySelector(".item-container"),
-  document.querySelector("#right"),
+  document.querySelector("#recycling-bin"),
+  document.querySelector("#landfill-bin"),
+  document.querySelector("#organic-bin"),
+  document.querySelector("#transfer-station"),
 ]);
+
+drake.on("drop", (el, target, source, sibling) => {
+  console.log("el: ", el);
+  console.log("target: ", target);
+  console.log("source: ", source);
+  console.log("sibling: ", sibling);
+});
